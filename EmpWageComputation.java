eaa8a2507a1140162cbee01c376f890c028f0173
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-14 20:59:14 +0530

    UC-5: Calculate Empployee Per Month Salary

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,39 +1,46 @@
 public class EmpWageComputation
 {
-	static final int isFullTime=1;
-	static final int isPartTime=2;
-	static final int EmpRatePerHour=20;
+	public static final int isFullTime=1;
+	public static final int isPartTime=2;
+	public static final int EmpRatePerHour=20;
+	public static final int NumOfWorkingDays=2;
 
-	public void calculateEmpWage()
+	public void calEmpWagePerMonth()
         {
 		int EmpHrs=0;
 		int EmpWage=0;
+		int TotalEmpWage=0;
 
-                int empCheck = (int)Math.floor(Math.random() *10) % 3;
-                switch(empCheck)
+		for(int day =0; day<NumOfWorkingDays; day++ )
 		{
-			case isFullTime :
-				System.out.println("Employee Is Full Time");
-				EmpHrs=8;
-				break;
 
-			case isPartTime:
-				System.out.println("Employee is Part Time");
-				EmpHrs=4;
-				break;
+                	int empCheck = (int)Math.floor(Math.random() *10) % 3;
+                	switch(empCheck)
+			{
+				case isFullTime :
+					System.out.println("Employee Is Full Time");
+					EmpHrs=8;
+					break;
 
-			default:
-				System.out.println("Employee Is Absent");
-				break;
-		}
-		EmpWage =EmpHrs * EmpRatePerHour;
-		System.out.println("Employee Wage:- "+EmpWage);
-         }
+				case isPartTime:
+					System.out.println("Employee is Part Time");
+					EmpHrs=4;
+					break;
 
+				default:
+					System.out.println("Employee Is Absent");
+					break;
+			}
+			EmpWage =EmpHrs * EmpRatePerHour;
+			TotalEmpWage += EmpWage;
+			System.out.println("Employee Wage:- "+EmpWage);
+        	 }
+		System.out.println("Total EmpWage:" + TotalEmpWage);
+	}
 
 	public static void  main(String args [])
 	{
 		EmpWageComputation emp = new EmpWageComputation();
-		emp.calculateEmpWage();
+		emp.calEmpWagePerMonth();
 	}
 }
