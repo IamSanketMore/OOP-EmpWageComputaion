29feedd7641562c3b51c3ab24bebf63e248871d6
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-14 15:45:16 +0530

    Welcome to Oop with Emp Wage Computation Problem

---
+++ EmpWageComputation.java
@@ -0,0 +1,7 @@
+public class EmpWageComputation
+{
+	public static void  main(String args [])
+	{
+		System.out.println("Welcome to Object Oriented Programming Concept Solve Emplyee Wage Computaion Problem ");
+	}
+}
