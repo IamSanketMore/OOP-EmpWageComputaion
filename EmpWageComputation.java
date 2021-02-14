d6266406c8b34a64644471e8670b4e18bd5a3825
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-14 22:21:55 +0530

    UC-6: Calculate Emp Wage TIll 100 hours

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,17 +3,20 @@ public class EmpWageComputation
 	public static final int isFullTime=1;
 	public static final int isPartTime=2;
 	public static final int EmpRatePerHour=20;
-	public static final int NumOfWorkingDays=2;
+	public static final int NumOfWorkingDays=20;
+	public static final int MaxHrsInMonth=100;
 
 	public void calEmpWagePerMonth()
         {
 		int EmpHrs=0;
 		int EmpWage=0;
 		int TotalEmpWage=0;
-
-		for(int day =0; day<NumOfWorkingDays; day++ )
+		int TotalEmpHrs=0;
+		int TotalWorkingDays=0;
+		while(TotalEmpHrs <= MaxHrsInMonth && TotalWorkingDays < NumOfWorkingDays)
 		{
 
+			TotalWorkingDays++;
                 	int empCheck = (int)Math.floor(Math.random() *10) % 3;
                 	switch(empCheck)
 			{
@@ -31,10 +34,10 @@ public class EmpWageComputation
 					System.out.println("Employee Is Absent");
 					break;
 			}
-			EmpWage =EmpHrs * EmpRatePerHour;
-			TotalEmpWage += EmpWage;
-			System.out.println("Employee Wage:- "+EmpWage);
+			TotalEmpHrs += EmpHrs;
+			System.out.println("Day:"+TotalWorkingDays+ "Emp Hr:"+EmpHrs);
         	 }
+		TotalEmpWage=TotalEmpHrs * EmpRatePerHour;
 		System.out.println("Total EmpWage:" + TotalEmpWage);
 	}
 
