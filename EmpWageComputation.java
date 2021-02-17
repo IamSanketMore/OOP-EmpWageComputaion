aea68c5790ce805742ad380a3bb87d7be755b384
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-17 19:02:26 +0530

    UC-9:-Save the Total Emp Wage For each Company

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,47 +1,66 @@
 public class EmpWageComputation
 {
-        public static final int FULL_TIME=1;
-        public static final int PART_TIME=2;
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
 
-        public static int ComputeEmpWage(String company,int empRatePerHr,int numOfWorkingDays,int maxHoursPerMonth)
-        {
-                int empHrs=0;
-                int totalEmpWage=0;
-                int totalEmpHrs=0;
-                int totalWorkingDays=0;
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
 
-                while(totalEmpHrs<=maxHoursPerMonth && totalWorkingDays<=numOfWorkingDays)
-                {
-                        totalWorkingDays++;
-                        int empCheck=(int)Math.floor(Math.random()*10)%3;
+	public EmpWageComputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+	{
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
 
-                        switch(empCheck)
-                        {
-                                case FULL_TIME:
-                                empHrs=8;
-                                break;
+	}
+	public void ComputeEmpWage()
+	{
+		int empHrs=0;
+		int totalEmpWage=0;
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
 
-                                case PART_TIME:
-                                empHrs=4;
-                                break;
+		while(totalEmpHrs<=maxHoursPerMonth && totalWorkingDays<=numOfWorkingDays)
+		{
+			totalWorkingDays++;
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
 
-                                default:
-                                empHrs=0;
-                        }
-                        totalEmpHrs+=empHrs;
+			switch(empCheck)
+			{
+			case FULL_TIME:
+				empHrs=8;
+				break;
+
+			case PART_TIME:
+				empHrs=4;
+				break;
+
+			default:
+				empHrs=0;
+			}
+			totalEmpHrs+=empHrs;
 			System.out.println("Day#:"+ totalWorkingDays + " Emp Hr:"+empHrs);
-                }
-                totalEmpWage = totalEmpHrs * empRatePerHr;
-                System.out.println("Company: "+company+" :: Total Employee Wage: "+totalEmpWage);
+		}
+		totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Wage For " +company+ " Company Employee Is "+totalEmpWage );
+	}
 
-		return totalEmpWage;
-        }
+	@Override
+	public String toString()
+	{
+		return "Total EmpWage for Company:"+company+ "is:"+totalEmpWage;
+	}
 
-        public static void main(String args [])
-        {
-                ComputeEmpWage("Facebook",14,34,52);
-                ComputeEmpWage("Google",24,35,34);
-                ComputeEmpWage("Twitter",12,30,45);
-        }
+	public static void main(String args [])
+	{
+		EmpWageComputation fb = new EmpWageComputation("Facebook",14,34,52);
+		fb.ComputeEmpWage();
+		EmpWageComputation go = new EmpWageComputation("Google",24,35,34);
+		go.ComputeEmpWage();
+	}
 
 }
